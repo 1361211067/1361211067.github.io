1c73c307b32cd0f6c720ecc6223605ad7f4daef0
Ray_Edir <124645254+1361211067@users.noreply.github.com>
2023-03-14 21:27:49 +0800

    Add files via upload
    第三次上传

---
+++ 音乐播放器/js/songsList.js
@@ -0,0 +1,38 @@
+var songsList = [
+    {
+        "name":'말해! 뭐해',
+        "auther":"K.Will",
+        "times":"03:61",
+        "like":false,
+        "imgSrc":"https://imgessl.kugou.com/stdmusic/20200527/20200527104808153179.jpg",
+        "src":"./src/말해! 뭐해 - K.Will.mp3"
+    },{
+        "name":'苦瓜',
+        "auther":"陈奕迅",
+        "times":"04:38",
+        "like":true,
+        "imgSrc":"https://imgessl.kugou.com/stdmusic/20210820/20210820124508256193.jpg",
+        "src":"./src/苦瓜-陈奕迅.mp3"
+    },{
+        "name":'乌鸦',
+        "auther":"许嵩",
+        "times":"05:29",
+        "like":false,
+        "imgSrc":"https://y.qq.com/music/photo_new/T002R300x300M000002C2T860Yo5Cr_1.jpg?max_age=2592000",
+        "src":"./src/乌鸦 - 许嵩.mp3"
+    },{
+        "name":'只对你有感觉',
+        "auther":"林俊杰",
+        "times":"04:27",
+        "like":true,
+        "imgSrc":"https://imgessl.kugou.com/stdmusic/20160908/20160908202449816878.jpg",
+        "src":"./src/只对你有感觉 - 林俊杰.mp3"
+    },{
+        "name":'Galway Girl',
+        "auther":"Ed Sheeran",
+        "times":"02:50",
+        "like":false,
+        "imgSrc":"https://imgessl.kugou.com/stdmusic/20200909/20200909112422386535.jpg",
+        "src":"./src/Galway Girl - Ed Sheeran.mp3"
+    }
+]
